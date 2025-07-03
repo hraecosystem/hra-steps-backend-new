d94c1613713b0d3f9e36c2ab5ccec1d225dcf451
rachad nait <rachadsalazar1996@gmail.com>
2025-07-03 18:16:10 +0400

    check reouts

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -29,7 +29,6 @@ router.post(
 
 
 // ADMIN only beyond this point
-router.use(authMiddleware);
 router.use((req, res, next) => {
   if (req.user.role !== 'admin') {
     return res.status(403).json({ message:'Admin access required' });
